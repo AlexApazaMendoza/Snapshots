f975d6336453344db8416d7be59ac5c26ed7d6e7
Alex Apaza <alex.apaza@tismart.com>
2022-09-01 15:41:22 -0500

    first commit

---
+++ app/src/main/java/com/example/snapshots/HomeFragment.kt
@@ -0,0 +1,153 @@
+package com.example.snapshots
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.snapshots.databinding.FragmentHomeBinding
+import com.example.snapshots.databinding.ItemSnapshotBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentHomeBinding
+
+    private lateinit var mFirebaseAdapter:FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>
+
+    private lateinit var mLayoutManager : RecyclerView.LayoutManager
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return mBinding.root
+        // Inflate the layout for this fragment
+        /*return inflater.inflate(R.layout.fragment_home, container, false)*/
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /***
+         * Adapter with Firebase
+         */
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+        val options = FirebaseRecyclerOptions.Builder<Snapshot>().setQuery(query,Snapshot::class.java).build()
+
+        mFirebaseAdapter = object :FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>(options){
+            private lateinit var mContext:Context
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                mContext = parent.context
+                val view = LayoutInflater.from(mContext).inflate(R.layout.item_snapshot, parent,false)
+                return SnapshotHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                val snapshot = getItem(position)
+
+                with(holder){
+                    setListener(snapshot)
+                    binding.tvTitle.text = snapshot.title
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(binding.imgPhoto)
+                }
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+                mBinding.progressBar.visibility = View.GONE
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext,error.message,Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        mLayoutManager = LinearLayoutManager(context)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mLayoutManager
+            adapter = mFirebaseAdapter
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAdapter.stopListening()
+    }
+
+    /***
+     * Holder para FirebaseUI
+     */
+    inner class SnapshotHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot){
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.snapshots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.snapshots.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setUpBottomNav()
+    }
+
+    private fun setUpBottomNav(){
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+
+        mFragmentManager
+            .beginTransaction()
+            .add(mBinding.hostFragment.id, profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment)
+            .commit()
+        mFragmentManager
+            .beginTransaction()
+            .add(mBinding.hostFragment.id, addFragment, AddFragment::class.java.name)
+            .hide(addFragment)
+            .commit()
+        mFragmentManager
+            .beginTransaction()
+            .add(mBinding.hostFragment.id, homeFragment, HomeFragment::class.java.name)
+            .commit()
+
+        mBinding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(homeFragment).commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+                R.id.action_add -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(addFragment).commit()
+                    mActiveFragment = addFragment
+                    true
+                }
+                R.id.action_profile -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(profileFragment).commit()
+                    mActiveFragment = profileFragment
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
