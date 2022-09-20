9bdb54b08f4f5384607a989df178d17a500edeb2
Alex Apaza <alex.apaza@tismart.com>
2022-09-20 12:37:24 -0500

    add firebase authentication

--- app/src/main/java/com/example/snapshots/MainActivity.kt
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.snapshots
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.snapshots.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +24,57 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mActiveFragment: Fragment
     private lateinit var mFragmentManager: FragmentManager
 
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private var mFirebaseAuth: FirebaseAuth? = null
+
+    private val responseAuthentication = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == Activity.RESULT_OK){
+            Toast.makeText(this, "Bienvenido...", Toast.LENGTH_SHORT).show()
+        } else {
+            if(it.data == null){
+                finish()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        setUpAuth()
         setUpBottomNav()
+    }
+
+    private fun setUpAuth() {
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if( user == null ){
+                val intent = Intent(
+                    AuthUI
+                        .getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                            mutableListOf(
+                                AuthUI.IdpConfig.EmailBuilder().build(),
+                                AuthUI.IdpConfig.GoogleBuilder().build()
+                            )
+                        ).build()
+                )
+                responseAuthentication.launch(intent)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth?.addAuthStateListener( mAuthListener )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth?.removeAuthStateListener( mAuthListener )
     }
 
     private fun setUpBottomNav(){
@@ -65,5 +121,9 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+    companion object {
+        val REQUEST_CODE_AUTH_SUCCESS = 100
+        val REQUEST_NAME_AUTH_SUCCESS = "REQUEST_AUTH"
     }
 }
